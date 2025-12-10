23778dc037ccb3b1298019e91b92a15d08481612
arnaudprc <arnaud.preci@gmail.com>
2025-12-10 10:26:23 +0100

    Fix typo in comment for the 'addition' function

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 # Exemple de programme Python avec commentaires basiques
 
-# On définit une fonction qui calcule la somme de deux nombres
+# On definit une fonction qui calcule la somme de deux nombres
 def addition(a, b):
     # On retourne le resultat de l'addition
     return a + b
